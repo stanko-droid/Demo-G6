94ccea99cf0214f33f256797d29b9425df9e8276
ludwigsevenheim-alt <Ludwigsevenheim@gmail.com>
2026-02-11 14:45:09 +0100

    Remove filter script with exposed secret

--- filter_script.py
+++
@@ -1,7 +0,0 @@
-def file_callback(filename, data):
-    """Remove secret from all files"""
-    return data.replace(b'[REMOVED]', b'[REMOVED]')
-
-def message_callback(message):
-    """Remove secret from commit messages"""
-    return message.replace(b'[REMOVED]', b'[REMOVED]')
